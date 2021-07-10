bb3abe4a662e8702b90b5fe65937ea71cba3820f
Jonathan García <jona03g97@gmail.com>
2021-07-09 18:39:07 -0600

    [Implementar autenticacion con redes sociales]: Implementando 0Auth2.0 con Google

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 require("./utils/auth/strategies/basic");
+require("./utils/auth/strategies/oauth");
 
 app.post("/auth/sign-in", async function (req, res, next) {
   const { rememberMe } = req.body;
@@ -105,6 +106,32 @@ app.delete("/user-movies/:userMovieId", async function (req, res, next) {
     next(error);
   }
 });
+
+app.get(
+  "/auth/google-oauth",
+  passport.authenticate("google-oauth", {
+    scope: ["email", "profile", "openid"],
+  })
+);
+
+app.get(
+  "/auth/google-oauth/callback",
+  passport.authenticate("google-oauth", { session: false }),
+  function (req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev,
+    });
+
+    res.status(200).json(user);
+  }
+);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
